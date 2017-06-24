104d8e145d54ace4c03cfa4167286e68431bd165
curiosul <bacchusstyle@yahoo.com>
2017-06-24 19:30:06 +0200

    Github source

--- Qt/libs/Communication/I2C.h
+++ Qt/libs/Communication/I2C.h
@@ -1,6 +1,8 @@
 #ifndef I2C_H
 #define I2C_H
 
+/// This may help: https://github.com/jrowberg/i2cdevlib
+
 /** I will implement this on Linux as it is nearly impossible to have a portable code.
   * This is the class which manage I2C connection. It implements ICommands interface.
   */
